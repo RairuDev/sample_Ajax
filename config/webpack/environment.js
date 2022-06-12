3442b8d4557c887c30cfd46c516c0d8ac9e2e466
Junich <junich0911@gmail.com>
2022-06-12 23:48:56 +0900

    first-commit

---
+++ config/webpack/environment.js
@@ -0,0 +1,13 @@
+const { environment } = require('@rails/webpacker')
+
+// 追加
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
+
+module.exports = environment
