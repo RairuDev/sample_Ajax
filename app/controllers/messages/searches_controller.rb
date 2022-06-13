33b2fc6de94e8774b0525173c0ad21ab3cc02b8a
Junich <junich0911@gmail.com>
2022-06-13 13:46:37 +0900

    sample-Ajax

---
+++ app/controllers/messages/searches_controller.rb
@@ -0,0 +1,11 @@
+class Messages::SearchesController < ApplicationController
+
+  def index
+    @messages = Message.where('title LIKE(?)', "%#{params[:title]}%")
+    # binding.pry
+    respond_to do |format|
+      format.html {redirect_to :root}
+      format.json {render json: @messages}
+    end
+  end
+end
--- app/javascript/packs/messages.js
+++ app/javascript/packs/messages.js
@@ -1,3 +1,24 @@
-$(document).ready(function() {
-  $("p").text("jQuery稼働テスト(稼働中)");
-});
+$(document).on('turbolinks:load', function (){
+  $(function(){
+    $('.js-text_field').on('keyup', function (){
+      // キーボードを入力したタイミングで以下の処理を実行する
+      var title = $.trim($(this).val());
+  
+      $.ajax({
+        type: 'GET',
+        url: '/messages/searches',
+        data: {title: title},
+        dataType: 'json'
+      })
+      .done(function(data){
+        $('.js-messages li').remove();
+  
+        $(data).each(function(i, message){
+          $('.js-messages').append(
+            `<li class="message"><a href="/messages/${message.id}">${message.title}</a></li>`
+          )
+        })
+      })
+    });
+  });
+})
